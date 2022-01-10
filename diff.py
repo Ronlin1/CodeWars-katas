3b2a04048c0ef4d7ed5be7e724849ce187cf27c5
Ronlin1 <ronlinx6@gmail.com>
2022-01-10 18:19:58 +0300

    push diff

---
+++ diff.py
@@ -0,0 +1,19 @@
+# Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+#
+# It should remove all values from list a, which are present in list b keeping their order.
+#
+# array_diff([1,2],[1]) == [2]
+# If a value is present in b, all of its occurrences must be removed from the other:
+#
+# array_diff([1,2,2,2,3],[2]) == [1,3]
+
+def array_diff(list_1, list_2):
+    diff = []
+    for value in list_1:
+        diff.append(value)
+        for v in list_2:
+            if v in diff:
+                diff.remove(v)
+    return diff
+
+print(array_diff([1,2,3,4],[1,2,6]))
